406a638c5808d04e57348da451a0c6176f3e4f9c
Satyam <bajpai1973satyam@gmail.com>
2024-03-14 14:55:29 +0530

    feat: add animation when price changes

--- src/components/StockHeader/StockHeader.tsx
+++ src/components/StockHeader/StockHeader.tsx
@@ -1,6 +1,7 @@
 import { CURRENCY_SYMBOL_MAPPER } from '@/src/constants';
 import { StockHeaderProps } from './stockHeader.types';
 import { MoveDown, MoveUp } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 function StockHeader({
   stockName,
@@ -9,13 +10,25 @@ function StockHeader({
   previousClosing,
   date,
 }: StockHeaderProps) {
+  const [price, setPrice] = useState<number>(21);
+  useEffect(() => {
+    function runInterval() {
+      const randomPrice = Number((Math.random() * 100).toFixed(2));
+      setPrice(randomPrice);
+    }
+    const interval = setInterval(runInterval, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   const currencySymbol: string =
     CURRENCY_SYMBOL_MAPPER[currency] || 'Unknown currency';
   const gainOrLossToday: number = Number(
     (currentPrice - previousClosing).toFixed(2)
   );
   const isPriceUp: boolean = gainOrLossToday > 0;
-
+  const priceSign = isPriceUp ? '' : '-';
   const priceBackgroundColor: string = isPriceUp
     ? 'bg-green-100'
     : 'bg-red-100';
@@ -35,17 +48,24 @@ function StockHeader({
         {stockName}
       </h2>
       <div className='stockMetrics flex items-center p-2 gap-4'>
-        <span className='text-4xl font-semibold'>
+        <span
+          key={`currentPrice-${price}`}
+          className={`text-4xl font-semibold animate-price-change`}
+        >
           {currencySymbol}
-          {currentPrice}
+          {price || currentPrice}
         </span>
         <span
           className={`flex h-10 rounded-md items-center w-20 px-1 ${priceBackgroundColor} ${priceTextColor}`}
         >
           {isPriceUp ? <MoveUp size={20} /> : <MoveDown size={20} />}
-          <span className='text-xl font-semibold'>{percentageChange}%</span>
+          <span className={`text-xl font-semibold`}>{percentageChange}%</span>
         </span>
-        <span className={`${priceTextColor} font-bold`}>{gainOrLossToday}</span>
+        <span className={`${priceTextColor} font-bold`}>
+          {' '}
+          {priceSign}
+          {price || gainOrLossToday}
+        </span>
       </div>
       <span className='ps-2'>{date}</span>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'price-change': 'price-change 0.6s ease-in-out',
+      },
+      keyframes: {
+        'price-change': {
+          '0%': {
+            transform: 'translateY(10px)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(-3px)',
+            opacity: '1',
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
