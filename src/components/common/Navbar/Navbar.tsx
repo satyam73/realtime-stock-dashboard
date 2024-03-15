aacc6a65799ebde811a50168ddb82660bbb664bb
Satyam <bajpai1973satyam@gmail.com>
2024-03-15 20:47:01 +0530

    fix: logo in navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar({searchRef}: NavbarProps) {
       className={'h-14 flex items-center border-b-2 shadow-sm border-dashed px-2'}
     >
       <span className='logo text-2xl font-bold max-sm:hidden'>Stocker</span>
-      <span className='logo text-2xl font-bold sm-visible'>S</span>
+      <span className='logo text-2xl font-bold sm:hidden'>S</span>
       <div className='mx-auto w-4/5'>
         <SearchBar
           searchRef={searchRef}
